e7e8e92d051d09c66956714d38f08556d97b0d19
Sotarev <nostrag@gmail.com>
2022-09-29 17:07:40 +0300

    car marks, models

---
+++ database/migrations/2022_09_29_133912_create_car_models_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\CarMark::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->string('year_start', 4)->nullable();
+            $table->string('year_end', 4)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_models');
+    }
+}
