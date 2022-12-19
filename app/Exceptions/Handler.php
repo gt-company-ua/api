f6c2a8df9347b10f25257d59e6bd0b38ee37b3d4
Sotarev <nostrag@gmail.com>
2022-12-19 13:20:45 +0300

    404 return json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,5 +41,47 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Define the response
+        $response = [
+            'errors' => 'Sorry, something went wrong.'
+        ];
+
+        // If the app is in debug mode
+        if (config('app.debug')) {
+            // Add the exception class name, message and stack trace to response
+            $response['exception'] = get_class($exception); // Reflection might be better here
+            $response['message'] = $exception->getMessage();
+            $response['trace'] = $exception->getTrace();
+        }
+
+        // Default response of 400
+        $status = 400;
+
+        // If this exception is an instance of HttpException
+        if ($this->isHttpException($exception)) {
+            // Grab the HTTP status code from the Exception
+            $status = $exception->getStatusCode();
+        }
+
+        if ($exception instanceof ModelNotFoundException)
+        {
+            $status = 404;
+        }
+
+        // Return a JSON response with the response array and status code
+        return $this->sendError($response, $status);
     }
 }
