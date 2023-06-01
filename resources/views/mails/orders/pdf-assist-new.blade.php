c31973dca71540fa1a43d79c6ee933a813b63c2c
Sotarev <nostrag@gmail.com>
2023-06-01 23:15:11 +0300

    update assist pdf

--- resources/views/mails/orders/pdf-assist-new.blade.php
+++ resources/views/mails/orders/pdf-assist-new.blade.php
@@ -7,22 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Договір публічної оферти</title>
 
+
     <style>
-        html {font-size:16px; line-height:1.3em}
+        @page {margin: 5rem 0 !important; padding: 5rem !important;}
+        html {font-size:13px; line-height:1.3em}
         html, body {padding:0; margin:0; min-width:320px}
+        body, * {font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;}
         * {line-height:1.3em; }
         .doc {margin: 0 auto; position:relative; overflow:hidden;}
         h1, h2, h3, h4, h5, h6 {text-align: center; margin-top:1.5em; margin-bottom:.5em; line-height: 1.2em; font-weight:600}
-        h1 {font-size:2.5rem}
-        h2 {font-size:2rem}
+        h1 {font-size:1.5rem}
+        h2 {font-size:1.25rem}
         img {display:inline-block; max-width:100%; max-height:100%; height:auto; line-height:1;}
         p + p {margin-top: 1rem}
         a {color:#009681; font-weight:500}
         a:hover, a:focus {color:#B3D73D}
         .num {font-weight:600; color:#111}
-        .doc__header {width:100%; position:relative}
+        .doc__header {width:100%; position:relative; margin-top:-5rem}
         .doc__header, .doc__header_bg, .doc__logo_wrap {height:7rem}
-        .doc__header_bg {position:absolute; top:0; left:0; right:0; bottom:0; background: url(https://api.greentravel.ua/storage/pdf/bg-waves.png) top left no-repeat; z-index:1; opacity:.33}
+        .doc__header_bg {position:absolute; top:0; left:0; right:0; background: url(https://api.greentravel.ua/storage/pdf/bg-waves.png) top left no-repeat; z-index:1; opacity:.33}
         .doc__logo_wrap {position:absolute; top:0; left:0; right:50%; background:#fff; padding:1rem; z-index:5}
         .doc__logo {min-width:10rem; width:30vw}
         .doc__logo_link, .doc__logo_wrap {display: flex; }
@@ -30,6 +33,7 @@
         body {background:#fff}
         .doc {margin: 3rem auto; background:#fff;}
         .doc__text {padding: 2rem 5rem}
+        @page { margin: 100px; }
         /* ====== */
     </style>
 </head>
