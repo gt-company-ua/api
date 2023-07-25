dbb8f7a3b6c2c23dd7e9f06d3945c73d39995103
Sotarev <nostrag@gmail.com>
2023-07-25 15:41:01 +0300

    fix ranges

--- app/Services/VzrService.php
+++ app/Services/VzrService.php
@@ -13,7 +13,7 @@ class VzrService
 {
     private $prices;
 
-    const AGE_RANGES = ['0', '4-15', '16-18', '19-60', '61-65', '66-75'];
+    const AGE_RANGES = ['0', '4-12', '13-17', '18-59', '60-65', '66-75'];
 
     /**
      * @throws Exception
