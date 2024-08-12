241c13c4c2813e13ae5d41143c10d8873e69d056
Yaroslav <nostrag@gmail.com>
2024-08-12 13:47:10 +0300

    offer mail template

--- resources/views/mails/orders/offer.blade.php
+++ resources/views/mails/orders/offer.blade.php
@@ -1,63 +1,105 @@
+
 <!DOCTYPE html>
-<html lang="ua">
+<html lang="ru">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Договір публічної оферти</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Email Template</title>
 
+    <style type="text/css">
+        #email {
+            font-family: Helvetica, Arial, sans-serif; font-size: 16px; color: #222721; line-height: 1.33; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;
+        }
 
-    <style>
-        @page {margin: 5rem 0 !important; padding: 5rem !important;}
-        html {font-size:13px; line-height:1.3em}
-        html, body {padding:0; margin:0; min-width:320px}
-        body, * {font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;}
-        * {line-height:1.3em; }
-        .doc {margin: 0 auto; position:relative; overflow:hidden;}
-        h1, h2, h3, h4, h5, h6 {text-align: center; margin-top:1.5em; margin-bottom:.5em; line-height: 1.2em; font-weight:600}
-        h1 {font-size:1.5rem}
-        h2 {font-size:1.25rem}
-        img {display:inline-block; max-width:100%; max-height:100%; height:auto; line-height:1;}
-        p + p {margin-top: 1rem}
-        a {color:#009681; font-weight:500}
-        a:hover, a:focus {color:#B3D73D}
-        .num {font-weight:600; color:#111}
-        .doc__header {width:100%; position:relative; margin-top:-5rem}
-        .doc__header, .doc__header_bg, .doc__logo_wrap {height:7rem}
-        .doc__header_bg {position:absolute; top:0; left:0; right:0; background: url(https://api.greentravel.ua/storage/pdf/bg-waves.png) top left no-repeat; z-index:1; opacity:.33}
-        .doc__logo_wrap {position:absolute; top:0; left:0; right:50%; background:#fff; padding:1rem; z-index:5}
-        .doc__logo {min-width:10rem; width:30vw}
-        .doc__logo_link, .doc__logo_wrap {display: flex; }
-        /* === это для оформления превью. Скорее всего для PDF надо будет удалить === */
-        body {background:#fff}
-        .doc {margin: 3rem auto; background:#fff;}
-        .doc__text {padding: 2rem 5rem}
-        @page { margin: 100px; }
-        /* ====== */
+        #email table, #email td {
+            border-collapse: collapse !important;
+        }
+
+        #email a {
+            text-decoration: none; color: #009383;
+        }
+
+        #email img {
+            display: inline-block; border:0; line-height:1; outline: none; text-decoration: none; max-width: 100%; height: auto;
+        }
+
+        #email .container {
+            max-width: 780px; width: 100%;
+        }
+
+        #email .content {
+            padding: 40px 40px;
+        }
+
+        #email h1 {
+            margin: 0 0 .75em; line-height:1.1;
+        }
+
+        #email p {
+            margin: 0 0 .5em;
+        }
+
+        #email code {
+            font-weight:600; padding:.25em .5em; border-radius:.5em; background:#eee; color:#009383;
+        }
+
+        #email .header {
+            text-align: center;
+        }
+
+        #email .footer {
+            text-align: center; font-size:.875em;
+        }
+
+        #email .footer__contacts {
+            color: #888;
+        }
     </style>
 </head>
-<body>
 
 
-
-<div class="doc">
-
-    <div class="doc__header">
-        <div class="doc__logo_wrap">
-            <a href="https://greentravel.ua" class="doc__logo_link" target="_blank">
-                <img src="https://api.greentravel.ua/storage/pdf/logo_text.png" alt="" class="doc__logo">
-            </a>
-        </div>
-        <div class="doc__header_bg"></div>
-    </div>
+<body id="email" style="margin: 0; padding: 0;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td style="padding: 20px;">
 
 
-    <div class="doc__text">
-        <h1>Персональна оферта</h1>
-        <p>Код підтвердження: {{ $code }}</p>
-    </div>
-</div>
+            <table class="container" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td class="header">
+                        <a href="https://greentravel.ua/ua/">
+                            <img width="780" src="https://greentravel.ua/email_files/email__header.jpg?v=3" alt="">
+                        </a>
+                    </td>
+                </tr>
 
+
+                <tr>
+                    <td class="content" style="text-align:center">
+                        <h1>Персональна оферта</h1>
+                        <p>Код підтвердження: <code>{{ $code }}</code></p>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td class="footer">
+                        <a href="https://greentravel.ua/ua/" style="display:inline-block; margin-bottom:20px">
+                            <img width="780" src="https://greentravel.ua/email_files/email__footer.jpg" alt="">
+                        </a>
+
+                        <div class="footer__contacts">
+                            <p style="font-weight:600"><a href="https://greentravel.ua/ua/">🌐 greentravel.ua</a> <span style="opacity:.3; padding:0 1em">|</span> 📱 <a href="tel:0800217675">0800217675</a> (безкоштовний)</p>
+                            <p>Твій страховий друг - green travel&trade;.<br>Працюємо для вас 24/7.</p>
+                        </div>
+
+                    </td>
+                </tr>
+            </table>
+
+
+        </td></tr>
+</table>
 </body>
+
 </html>
